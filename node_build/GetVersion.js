4db43d141d5cf13fc044e552a3923861e92cf152
Cameron Garnham <da2ce7@gmail.com>
2015-11-25 16:27:49 +0800

    format: run js-beautify on node_build

--- node_build/GetVersion.js
+++ node_build/GetVersion.js
@@ -1,8 +1,8 @@
 var nThen = require('nthen');
 var Spawn = require('child_process').spawn;
 
-var getversion = module.exports = function (callback) {
-    nThen(function (waitFor) {
+var getversion = module.exports = function(callback) {
+    nThen(function(waitFor) {
         var child = Spawn('git', ['describe', '--always', '--dirty', '--tags']);
         child.stdout.on('data', function(data) {
             callback(data);
@@ -10,8 +10,7 @@ var getversion = module.exports = function (callback) {
         child.stderr.on('data', function(data) {
             throw new Error("git error: " + data);
         });
-        child.on('close', function(code) {
-        });
+        child.on('close', function(code) {});
         child.on('error', function(err) {
             throw new Error("git error: " + err);
         });
